250747c6fa5c98b8c968ecf7ecb92819a9336afe
Umair Quais <53208396+uquais@users.noreply.github.com>
2020-04-09 02:07:44 +0530

    SpringORM

---
+++ springorm/src/main/java/com/umair/spring/springorm/product/dao/ProductDao.java
@@ -0,0 +1,9 @@
+ package com.umair.spring.springorm.product.dao;
+
+import com.umair.spring.springorm.entity.Product;
+
+public interface ProductDao {
+	
+	int create(Product product);
+
+}
---
+++ springorm/src/main/java/com/umair/spring/springorm/product/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,13 @@
+package com.umair.spring.springorm.product.dao.impl;
+
+import com.umair.spring.springorm.entity.Product;
+import com.umair.spring.springorm.product.dao.ProductDao;
+
+public class ProductDaoImpl implements ProductDao {
+
+	@Override
+	public int create(Producti product) {
+		return 0;
+	}
+
+}
